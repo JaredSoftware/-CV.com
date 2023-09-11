13a0ecec6e1a293588c450da090d9a817e247011
Jared Vargas <jaredwesley27@hotmail.com>
2023-09-11 15:10:48 -0500

    change everiting

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <cryavionet />
+    <button @click="handleClick">Haz clic aquí</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    handleClick() {
+      console.log('¡Haz hecho clic en el botón!')
+    }
+  }
+}
+</script>
