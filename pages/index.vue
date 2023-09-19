6ad45f59b38aedce28674b6ddc9419f4f91ed523
Jared Vargas <jaredwesley27@hotmail.com>
2023-09-19 08:47:45 -0500

    cambiando expereiencia

--- pages/index.vue
+++ pages/index.vue
@@ -138,6 +138,21 @@
           facilitar soluciones a clientes
         </p>
       </li>
+      <li class="mb-10 ml-4">
+        <div
+          class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+        ></div>
+        <time
+          class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+          >Mayo 2022 | Presente</time
+        >
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+          FreeLance
+        </h3>
+        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+          Desarrollo para variedad de clientes, brindando soporte a desarrollo y soluciones a necesidades puntuales.
+        </p>
+      </li>
     </ol>
   </div>
   <div
